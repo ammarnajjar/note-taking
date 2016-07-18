cca7d524a722bb24edd6444276b14a8d73b4cc03
Ammar Najjar <najjarammar@gmail.com>
2016-07-18 14:17:44 +0200

    Add simple wtf form.

--- app.py
+++ app.py
@@ -10,16 +10,27 @@ from flask import Flask
 from flask import render_template
 from flask_bootstrap import Bootstrap
 
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class IdeaForm(Form):
+     idea = StringField('Idea', validators=[DataRequired()])
+     submit_button = SubmitField('Add idea')
+
 def create_app():
     app = Flask(__name__)
     Bootstrap(app)
+    # in real app use Flask-Appconfig
+    app.config['SECRET_KEY'] = 'devkey'
     return app
 
 app = create_app()
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    form = IdeaForm()
+    return render_template("index.html", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
