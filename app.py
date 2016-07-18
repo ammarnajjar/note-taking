b354dd9f2b3bb2e569e5403286911effa86f09b2
Ammar Najjar <najjarammar@gmail.com>
2016-07-18 14:30:44 +0200

    form just returns the input idea back to the page.

--- app.py
+++ app.py
@@ -27,10 +27,13 @@ def create_app():
 
 app = create_app()
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def hello():
     form = IdeaForm()
-    return render_template("index.html", form=form)
+    submitted_idea = '<No idea submitted>'
+    if form.validate_on_submit():
+        submitted_idea = form.idea.data
+    return render_template('index.html', form=form, submitted_idea=submitted_idea)
 
 if __name__ == "__main__":
     app.run(debug=True)
